cdfe2942ab381e8c77a9d8191e0a5345c29e3aca
Kevin Wade <kevin@kevinwade.name>
2017-06-22 09:06:09 -0500

    Recompile js

--- examples/index.js
+++ examples/index.js
@@ -1,76 +1,207 @@
-import React, {Component} from 'react';
-import OlayReact from 'olay-react';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'react', 'olay-react'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('react'), require('olay-react'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.React, global.OlayReact);
+    global.index = mod.exports;
+  }
+})(this, function (exports, _react, _olayReact) {
+  'use strict';
 
-class UserListItem extends Component {
-  handleColorChange(ev) {
-    this.props.onColorChange(ev.target.value);
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _react2 = _interopRequireDefault(_react);
+
+  var _olayReact2 = _interopRequireDefault(_olayReact);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
   }
 
-  render() {
-    const {color, name} = this.props.user;
-    return (
-      <div style={{color}}>
-        <span className='user-name'>{name}</span>
-        <input onChange={::this.handleColorChange} value={color} />
-        <input type='button' onClick={this.props.onSelect} value='Olay!' />
-      </div>
-    );
-  }
-}
-
-export default class UsersList extends Component {
-  state = {
-    users: this.props.users
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
   }
 
-  closeOlay() {
-    this.setState({currentUserIndex: null});
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  function _possibleConstructorReturn(self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
   }
 
-  handleColorChange(i, color) {
-    const {onColorChange} = this.props;
-    const {users} = this.state;
-    users[i].color = color;
-    this.setState({users});
-    if (onColorChange) onColorChange(i, color);
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
   }
 
-  handleSelect(i) {
-    this.setState({currentUserIndex: i});
-  }
+  var UserListItem = function (_Component) {
+    _inherits(UserListItem, _Component);
 
-  renderUserListItem(user, i) {
-    return (
-      <UserListItem
-        key={i}
-        index={i}
-        user={user}
-        onColorChange={this.handleColorChange.bind(this, i)}
-        onSelect={this.handleSelect.bind(this, i)}
-      />
-    );
-  }
+    function UserListItem() {
+      _classCallCheck(this, UserListItem);
 
-  renderCurrentUser() {
-    const {currentUserIndex, users} = this.state;
-    const user = users[currentUserIndex];
-    if (!user) return;
-    return (
-      <OlayReact close={::this.closeOlay}>
-        <div className='current-user' style={{color: user.color}}>
-          <h1>{`${user.name}'s Olay!`}</h1>
-          <UsersList users={users} onColorChange={::this.handleColorChange} />
-        </div>
-      </OlayReact>
-    );
-  }
+      return _possibleConstructorReturn(this, (UserListItem.__proto__ || Object.getPrototypeOf(UserListItem)).apply(this, arguments));
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.users.map(::this.renderUserListItem)}
-        {this.renderCurrentUser()}
-      </div>
-    );
-  }
-};
+    _createClass(UserListItem, [{
+      key: 'handleColorChange',
+      value: function handleColorChange(ev) {
+        this.props.onColorChange(ev.target.value);
+      }
+    }, {
+      key: 'render',
+      value: function render() {
+        var _props$user = this.props.user,
+            color = _props$user.color,
+            name = _props$user.name;
+
+        return _react2.default.createElement(
+          'div',
+          { style: { color: color } },
+          _react2.default.createElement(
+            'span',
+            { className: 'user-name' },
+            name
+          ),
+          _react2.default.createElement('input', { onChange: this.handleColorChange.bind(this), value: color }),
+          _react2.default.createElement('input', { type: 'button', onClick: this.props.onSelect, value: 'Olay!' })
+        );
+      }
+    }]);
+
+    return UserListItem;
+  }(_react.Component);
+
+  var UsersList = function (_Component2) {
+    _inherits(UsersList, _Component2);
+
+    function UsersList() {
+      var _ref;
+
+      var _temp, _this2, _ret;
+
+      _classCallCheck(this, UsersList);
+
+      for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      return _ret = (_temp = (_this2 = _possibleConstructorReturn(this, (_ref = UsersList.__proto__ || Object.getPrototypeOf(UsersList)).call.apply(_ref, [this].concat(args))), _this2), _this2.state = {
+        users: _this2.props.users
+      }, _temp), _possibleConstructorReturn(_this2, _ret);
+    }
+
+    _createClass(UsersList, [{
+      key: 'closeOlay',
+      value: function closeOlay() {
+        this.setState({ currentUserIndex: null });
+      }
+    }, {
+      key: 'handleColorChange',
+      value: function handleColorChange(i, color) {
+        var onColorChange = this.props.onColorChange;
+        var users = this.state.users;
+
+        users[i].color = color;
+        this.setState({ users: users });
+        if (onColorChange) onColorChange(i, color);
+      }
+    }, {
+      key: 'handleSelect',
+      value: function handleSelect(i) {
+        this.setState({ currentUserIndex: i });
+      }
+    }, {
+      key: 'renderUserListItem',
+      value: function renderUserListItem(user, i) {
+        return _react2.default.createElement(UserListItem, {
+          key: i,
+          index: i,
+          user: user,
+          onColorChange: this.handleColorChange.bind(this, i),
+          onSelect: this.handleSelect.bind(this, i)
+        });
+      }
+    }, {
+      key: 'renderCurrentUser',
+      value: function renderCurrentUser() {
+        var _state = this.state,
+            currentUserIndex = _state.currentUserIndex,
+            users = _state.users;
+
+        var user = users[currentUserIndex];
+        if (!user) return;
+        return _react2.default.createElement(
+          _olayReact2.default,
+          { close: this.closeOlay.bind(this) },
+          _react2.default.createElement(
+            'div',
+            { className: 'current-user', style: { color: user.color } },
+            _react2.default.createElement(
+              'h1',
+              null,
+              user.name + '\'s Olay!'
+            ),
+            _react2.default.createElement(UsersList, { users: users, onColorChange: this.handleColorChange.bind(this) })
+          )
+        );
+      }
+    }, {
+      key: 'render',
+      value: function render() {
+        return _react2.default.createElement(
+          'div',
+          null,
+          this.state.users.map(this.renderUserListItem.bind(this)),
+          this.renderCurrentUser()
+        );
+      }
+    }]);
+
+    return UsersList;
+  }(_react.Component);
+
+  exports.default = UsersList;
+  ;
+});
--- olay-react.js
+++ olay-react.js
@@ -1,122 +1,324 @@
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-
-const FOCUSABLE = [
-  '[contenteditable]',
-  '[tabindex]',
-  'a',
-  'button',
-  'embed',
-  'iframe',
-  'input',
-  'object',
-  'select',
-  'textarea'
-].join(', ');
-
-if (typeof document !== 'undefined') {
-  document.addEventListener('keydown', ev => {
-    if (!active.length) return;
-
-    const last = active[active.length - 1];
-    const keys = last.props.closeOnKeys || [];
-    const which = ev.which;
-    for (let i = 0, l = keys.length; i < l; ++i) {
-      if (which !== keys[i]) continue;
-
-      last.props.close();
-      return false;
-    }
-  });
-}
-const active = [];
-
-const activate = component => {
-  if (active.indexOf(component) !== -1) return;
-
-  active.push(component);
-  restrictFocusTo(component.remote);
-  document.body.classList.add('olay-active');
-};
-
-const deactivate = component => {
-  const i = active.indexOf(component);
-  if (i === -1) return;
-
-  active.splice(i, 1);
-  if (active.length) {
-    restrictFocusTo(active[active.length - 1].remote);
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'react-transition-group/CSSTransitionGroup', 'prop-types', 'react', 'react-dom'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('react-transition-group/CSSTransitionGroup'), require('prop-types'), require('react'), require('react-dom'));
   } else {
-    restrictFocusTo(document.body);
-    const {body} = document;
-    body.classList.remove('olay-active');
-    if (!body.className) body.removeAttribute('class');
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.CSSTransitionGroup, global.PropTypes, global.React, global.ReactDOM);
+    global.OlayReact = mod.exports;
   }
-};
+})(this, function (exports, _CSSTransitionGroup, _propTypes, _react, _reactDom) {
+  'use strict';
 
-const getTabIndex = el => {
-  const {attributes} = el;
-  for (let i = 0, l = attributes.length; i < l; ++i) {
-    const {name, value} = attributes[i];
-    if (name === 'tabindex') return value;
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _CSSTransitionGroup2 = _interopRequireDefault(_CSSTransitionGroup);
+
+  var _propTypes2 = _interopRequireDefault(_propTypes);
+
+  var _react2 = _interopRequireDefault(_react);
+
+  var _reactDom2 = _interopRequireDefault(_reactDom);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
   }
-};
 
-const saveTabIndex = el => {
-  if ('olaySavedTabIndex' in el) return;
+  function _objectWithoutProperties(obj, keys) {
+    var target = {};
 
-  el.olaySavedTabIndex = getTabIndex(el);
-};
+    for (var i in obj) {
+      if (keys.indexOf(i) >= 0) continue;
+      if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
+      target[i] = obj[i];
+    }
 
-const restoreTabIndex = el => {
-  if (!('olaySavedTabIndex' in el)) return;
+    return target;
+  }
 
-  if (el.olaySavedTabIndex) el.tabIndex = el.olaySavedTabIndex;
-  else el.removeAttribute('tabindex');
-  delete el.olaySavedTabIndex;
-};
-
-const restrictFocusTo = parent => {
-  const els = document.body.querySelectorAll(FOCUSABLE);
-  for (let i = 0, l = els.length; i < l; ++i) {
-    const el = els[i];
-    if (parent.contains(el)) {
-      restoreTabIndex(els[i]);
-    } else {
-      saveTabIndex(els[i]);
-      els[i].tabIndex = -1;
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
     }
   }
-};
 
-const setFocus = el => {
-  if (!el) return;
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
 
-  if (el.tabIndex >= 0) return el.focus();
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
 
-  saveTabIndex(el);
-  el.tabIndex = 0;
-  el.focus();
-  restoreTabIndex(el);
-};
+  function _possibleConstructorReturn(self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
 
-export default class extends Component {
-  static propTypes = {
-    children: PropTypes.any,
-    close: PropTypes.func.isRequired,
-    closeOnClick: PropTypes.bool,
-    closeOnKeys: PropTypes.arrayOf(PropTypes.number),
-    component: PropTypes.node,
-    transitionAppear: PropTypes.bool,
-    transitionAppearTimeout: PropTypes.number,
-    transitionEnterTimeout: PropTypes.number,
-    transitionLeaveTimeout: PropTypes.number,
-    transitionName: PropTypes.string
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
   }
 
-  static defaultProps = {
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  }
+
+  var FOCUSABLE = ['[contenteditable]', '[tabindex]', 'a', 'button', 'embed', 'iframe', 'input', 'object', 'select', 'textarea'].join(', ');
+
+  if (typeof document !== 'undefined') {
+    document.addEventListener('keydown', function (ev) {
+      if (!active.length) return;
+
+      var last = active[active.length - 1];
+      var keys = last.props.closeOnKeys || [];
+      var which = ev.which;
+      for (var i = 0, l = keys.length; i < l; ++i) {
+        if (which !== keys[i]) continue;
+
+        last.props.close();
+        return false;
+      }
+    });
+  }
+  var active = [];
+
+  var activate = function activate(component) {
+    if (active.indexOf(component) !== -1) return;
+
+    active.push(component);
+    restrictFocusTo(component.remote);
+    document.body.classList.add('olay-active');
+  };
+
+  var deactivate = function deactivate(component) {
+    var i = active.indexOf(component);
+    if (i === -1) return;
+
+    active.splice(i, 1);
+    if (active.length) {
+      restrictFocusTo(active[active.length - 1].remote);
+    } else {
+      restrictFocusTo(document.body);
+      var _document = document,
+          body = _document.body;
+
+      body.classList.remove('olay-active');
+      if (!body.className) body.removeAttribute('class');
+    }
+  };
+
+  var getTabIndex = function getTabIndex(el) {
+    var attributes = el.attributes;
+
+    for (var i = 0, l = attributes.length; i < l; ++i) {
+      var _attributes$i = attributes[i],
+          name = _attributes$i.name,
+          value = _attributes$i.value;
+
+      if (name === 'tabindex') return value;
+    }
+  };
+
+  var saveTabIndex = function saveTabIndex(el) {
+    if ('olaySavedTabIndex' in el) return;
+
+    el.olaySavedTabIndex = getTabIndex(el);
+  };
+
+  var restoreTabIndex = function restoreTabIndex(el) {
+    if (!('olaySavedTabIndex' in el)) return;
+
+    if (el.olaySavedTabIndex) el.tabIndex = el.olaySavedTabIndex;else el.removeAttribute('tabindex');
+    delete el.olaySavedTabIndex;
+  };
+
+  var restrictFocusTo = function restrictFocusTo(parent) {
+    var els = document.body.querySelectorAll(FOCUSABLE);
+    for (var i = 0, l = els.length; i < l; ++i) {
+      var el = els[i];
+      if (parent.contains(el)) {
+        restoreTabIndex(els[i]);
+      } else {
+        saveTabIndex(els[i]);
+        els[i].tabIndex = -1;
+      }
+    }
+  };
+
+  var setFocus = function setFocus(el) {
+    if (!el) return;
+
+    if (el.tabIndex >= 0) return el.focus();
+
+    saveTabIndex(el);
+    el.tabIndex = 0;
+    el.focus();
+    restoreTabIndex(el);
+  };
+
+  var _class = function (_Component) {
+    _inherits(_class, _Component);
+
+    function _class() {
+      _classCallCheck(this, _class);
+
+      return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
+    }
+
+    _createClass(_class, [{
+      key: 'componentWillMount',
+      value: function componentWillMount() {
+        requestAnimationFrame(this.mountRemote.bind(this));
+      }
+    }, {
+      key: 'componentDidUpdate',
+      value: function componentDidUpdate() {
+        this.renderRemote();
+      }
+    }, {
+      key: 'componentWillUnmount',
+      value: function componentWillUnmount() {
+        var _this2 = this;
+
+        this.renderRemote({
+          unmount: true,
+          cb: function cb() {
+            return setTimeout(_this2.unmountRemote.bind(_this2), _this2.props.transitionLeaveTimeout);
+          }
+        });
+      }
+    }, {
+      key: 'mountRemote',
+      value: function mountRemote() {
+        var _this3 = this;
+
+        this.prevActiveElement = document.activeElement;
+        document.body.appendChild(this.remote = document.createElement('div'));
+        activate(this);
+        this.renderRemote({ cb: function cb() {
+            return setFocus(_this3.cell);
+          } });
+      }
+    }, {
+      key: 'unmountRemote',
+      value: function unmountRemote() {
+        requestAnimationFrame(this.reallyUnmountRemote.bind(this));
+      }
+    }, {
+      key: 'reallyUnmountRemote',
+      value: function reallyUnmountRemote() {
+        _reactDom2.default.unmountComponentAtNode(this.remote);
+        deactivate(this);
+        document.body.removeChild(this.remote);
+        setFocus(this.prevActiveElement);
+      }
+    }, {
+      key: 'handleClick',
+      value: function handleClick(ev) {
+        var _props = this.props,
+            close = _props.close,
+            closeOnClick = _props.closeOnClick;
+
+        if (!closeOnClick) return;
+
+        var target = ev.target;
+        var els = [].slice.call(_reactDom2.default.findDOMNode(this.cell).children);
+        if (els.some(function (el) {
+          return el.contains(target);
+        })) return;
+        close();
+        ev.stopPropagation();
+      }
+    }, {
+      key: 'renderRemote',
+      value: function renderRemote() {
+        var _this4 = this;
+
+        var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+            _ref$unmount = _ref.unmount,
+            unmount = _ref$unmount === undefined ? false : _ref$unmount,
+            cb = _ref.cb;
+
+        if (!this.remote) return;
+
+        var _props2 = this.props,
+            children = _props2.children,
+            close = _props2.close,
+            closeOnClick = _props2.closeOnClick,
+            closeOnKeys = _props2.closeOnKeys,
+            rest = _objectWithoutProperties(_props2, ['children', 'close', 'closeOnClick', 'closeOnKeys']);
+
+        _reactDom2.default.render(_react2.default.createElement(
+          _CSSTransitionGroup2.default,
+          rest,
+          unmount ? null : _react2.default.createElement(
+            'div',
+            { className: 'olay-container', onClick: this.handleClick.bind(this) },
+            _react2.default.createElement(
+              'div',
+              { className: 'olay-table' },
+              _react2.default.createElement(
+                'div',
+                { ref: function ref(c) {
+                    return _this4.cell = c;
+                  }, className: 'olay-cell' },
+                children
+              )
+            )
+          )
+        ), this.remote, cb);
+      }
+    }, {
+      key: 'render',
+      value: function render() {
+        return null;
+      }
+    }]);
+
+    return _class;
+  }(_react.Component);
+
+  _class.propTypes = {
+    children: _propTypes2.default.any,
+    close: _propTypes2.default.func.isRequired,
+    closeOnClick: _propTypes2.default.bool,
+    closeOnKeys: _propTypes2.default.arrayOf(_propTypes2.default.number),
+    component: _propTypes2.default.node,
+    transitionAppear: _propTypes2.default.bool,
+    transitionAppearTimeout: _propTypes2.default.number,
+    transitionEnterTimeout: _propTypes2.default.number,
+    transitionLeaveTimeout: _propTypes2.default.number,
+    transitionName: _propTypes2.default.string
+  };
+  _class.defaultProps = {
     closeOnKeys: [27],
     closeOnClick: true,
     component: 'div',
@@ -125,74 +327,6 @@ export default class extends Component {
     transitionEnterTimeout: 250,
     transitionLeaveTimeout: 250,
     transitionName: 'olay-fade'
-  }
-
-  componentWillMount() {
-    requestAnimationFrame(::this.mountRemote);
-  }
-
-  componentDidUpdate() {
-    this.renderRemote();
-  }
-
-  componentWillUnmount() {
-    this.renderRemote({
-      unmount: true,
-      cb: () =>
-        setTimeout(::this.unmountRemote, this.props.transitionLeaveTimeout)
-    });
-  }
-
-  mountRemote() {
-    this.prevActiveElement = document.activeElement;
-    document.body.appendChild(this.remote = document.createElement('div'));
-    activate(this);
-    this.renderRemote({cb: () => setFocus(this.cell)});
-  }
-
-  unmountRemote() {
-    requestAnimationFrame(::this.reallyUnmountRemote);
-  }
-
-  reallyUnmountRemote() {
-    ReactDOM.unmountComponentAtNode(this.remote);
-    deactivate(this);
-    document.body.removeChild(this.remote);
-    setFocus(this.prevActiveElement);
-  }
-
-  handleClick(ev) {
-    const {close, closeOnClick} = this.props;
-    if (!closeOnClick) return;
-
-    const target = ev.target;
-    const els = [].slice.call(ReactDOM.findDOMNode(this.cell).children);
-    if (els.some(el => el.contains(target))) return;
-    close();
-    ev.stopPropagation();
-  }
-
-  renderRemote({unmount = false, cb} = {}) {
-    if (!this.remote) return;
-
-    const {children, close, closeOnClick, closeOnKeys, ...rest} = this.props;
-    ReactDOM.render(
-      <CSSTransitionGroup {...rest}>
-        {
-          unmount ? null :
-          <div className='olay-container' onClick={::this.handleClick}>
-            <div className='olay-table'>
-              <div ref={c => this.cell = c} className='olay-cell'>
-                {children}
-              </div>
-            </div>
-          </div>
-        }
-      </CSSTransitionGroup>
-    , this.remote, cb);
-  }
-
-  render() {
-    return null;
-  }
-}
+  };
+  exports.default = _class;
+});
